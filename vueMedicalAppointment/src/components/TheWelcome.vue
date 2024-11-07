61f3c455393c8ec626d0660358b03717e8877806
liliang <1606599515@qq.com>
2024-11-07 19:15:41 +0800

    前端架子

---
+++ vueMedicalAppointment/src/components/TheWelcome.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+    <div class="welcome">
+        <h1>登录/注册</h1>
+    </div>
+</template>
---
+++ vueMedicalAppointment/src/views/LoginView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <div><TheWelcome /></div>
+
+  </main>
+</template>
