f87365d831c01e4c42d5f688d26e8e67b939cd3b
bushiheshi <2252723@tongji.edu.cn>
2024-12-19 18:24:32 +0800

    修改了路由配置文件

--- front/front/src/router/index.js
+++ front/front/src/router/index.js
@@ -4,25 +4,14 @@ import {
 } from 'vue-router'
 import store from '@/store/index.js'
 
-
+// 使用动态导入
 const AdminLayout = () => import('@/layouts/AdminLayout.vue')
 const managerHome = () => import('../views/manager/managerHome.vue')
 const login = () => import('../views/patient/login.vue')
 const patientHome = () => import('../views/patient/Layout/patientHome.vue')
 const PatientDetail = () => import('@/views/manager/patientDetail.vue')
 const DoctorManagement = () => import('@/views/manager/DoctorManagement.vue')
-
-import managerHome from '../views/manager/managerHome.vue'
-import login from '../views/patient/login.vue'
-
-import patientHome from '../views/patient/Layout/patientHome.vue'
-
-import PatientDetail from '@/views/manager/patientDetail.vue'
-
-
-import doctor from '../views/doctor/doctor.vue'
-
-
+const doctor = () => import('../views/doctor/doctor.vue')
 
 const routers = [
     {
@@ -30,13 +19,30 @@ const routers = [
         redirect: '/managerHome'
     },
     {
-
         path: '/login',
         name: 'login',
         component: login,
         meta: {
             title: '登录页面',
             requiresAuth: false
+        }
+    },
+    {
+        path: '/patientHome',
+        name: 'patientHome',
+        component: patientHome,
+        meta: {
+            title: '患者页面',
+            requiresAuth: true
+        }
+    },
+    {
+        path: '/doctor',
+        name: 'doctor',
+        component: doctor,
+        meta: {
+            title: '医生页面',
+            requiresAuth: true
         }
     },
     {
@@ -74,39 +80,6 @@ const routers = [
     }
 ];
 
-
-        path:"/patientHome",
-        name:"patientHome",
-        component: patientHome,
-        meta:{
-            title:"患者页面",
-            requiresAuth: true
-        }
-    },
-    {
-
-        path: '/manager/patient/:id',
-        name: 'PatientDetail',
-        component: PatientDetail,
-        meta: {
-            requiresAuth: true,
-            title: '患者详情'
-        }
-    }
-
-        path:"/doctor",
-        name:"doctor",
-        component:doctor,
-        meta:{
-            title: "医生页面", 
-            requiresAuth: true // 表示该路由需要用户认证
-        }
-    }
-
-
-]
-
-
 const router = createRouter({
     history: createWebHashHistory(),
     routes: routers
@@ -127,11 +100,12 @@ export { router };
 /*
 router.beforeEach((to, from, next) => {
     const isAuthenticated = store.state.user || store.state.merchant ||store.state.rider;
-    const requiresAuth = to.meta.requiresAuth; // 确保获取到目标路由的 requiresAuth 属性
+    const requiresAuth = to.meta.requiresAuth;
     if (requiresAuth && !isAuthenticated) {
-        next({ path: '/login' }); // 重定向到登录
+        next({ path: '/login' });
     } else {
-        next(); // 继续导航
+        next();
     }
-});*/
+});
+*/
 
